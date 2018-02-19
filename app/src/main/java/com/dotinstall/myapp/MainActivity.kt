d94befaec100f1db38f24b38271cc4877849b45c
kyam <kyam02460131@gmail.com>
2018-02-19 22:24:13 +0900

    finish Androidアプリ開発入門

--- app/src/main/java/com/dotinstall/myapp/MainActivity.kt
+++ app/src/main/java/com/dotinstall/myapp/MainActivity.kt
@@ -2,11 +2,18 @@ package com.dotinstall.myapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //親クラスの処理を呼んでいる
         setContentView(R.layout.activity_main)
+        //レイアウト中のactivity_mainをContentViewにセットしている。
+    }
+    fun changeTextView(view:View){
+        messageTextView.text="Hello there!!　Again!"
     }
 }
